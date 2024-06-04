3f2aaef0c0a7713da757175055658de59fbb87e4
Scott Perkins <whatisbyandby@gmail.com>
2024-06-03 22:22:08 -0600

    Added JSON serializer

--- src/json/json.cpp
+++ src/json/json.cpp
@@ -14,6 +14,15 @@ ThermostatError serialize(char *buffer, size_t len, ThermostatState *state)
     buffer = json_objOpen( buffer, NULL, &len);
     buffer = json_double( buffer, "currentTemperature", state->currentTemperature, &len);
     buffer = json_double( buffer, "currentHumidity", state->currentHumidity, &len);
+    buffer = json_double( buffer, "targetTemperature", state->targetTemperature, &len);
+    buffer = json_double( buffer, "currentTemperatureStandardUnits", state->currentTemperatureStandardUnits, &len);
+    buffer = json_double( buffer, "targetTemperatureStandardUnits", state->targetTemperatureStandardUnits, &len);
+    buffer = json_double( buffer, "temperatureRange", state->temperatureRange, &len);
+    buffer = json_str( buffer, "temperatureState", temperatureStateToString(state->temperatureState), &len);
+    buffer = json_str( buffer, "temperatureUnits", temperatureUnitsToString(state->temperatureUnits), &len);
+    buffer = json_str( buffer, "hvacState", hvacStateToString(state->hvacState), &len);
+    buffer = json_str( buffer, "mode", thermostatModeToString(state->mode), &len);
+    buffer = json_str( buffer, "error", thermostatErrorToString(state->error), &len);
     buffer = json_objClose( buffer, &len);
     buffer = json_end( buffer, &len);
     
--- test/json/json_test.cpp
+++ test/json/json_test.cpp
@@ -19,12 +19,34 @@ TEST(JsonTestGroup, SerializeThermostatState)
     ThermostatState state;
     state.currentTemperature = 22.1;
     state.currentHumidity = 50.1;
+    state.targetTemperature = 22.1;
+    state.currentTemperatureStandardUnits = 22.1;
+    state.targetTemperatureStandardUnits = 22.1;
+    state.temperatureRange = 1.1;
+    state.temperatureState = OVER_TEMPERATURE;
+    state.temperatureUnits = CELSIUS;
+    state.hvacState = HEATER_ON;
+    state.mode = HEATING;
+    state.error = THERMOSTAT_OK;
     
     size_t len = 1024;
     char buffer[len];
 
     serialize(buffer, len, &state);
+
+    STRCMP_EQUAL("{\"currentTemperature\":22.1,"
+    "\"currentHumidity\":50.1,"
+    "\"targetTemperature\":22.1,"
+    "\"currentTemperatureStandardUnits\":22.1,"
+    "\"targetTemperatureStandardUnits\":22.1,"
+    "\"temperatureRange\":1.1,"
+    "\"temperatureState\":\"OVER_TEMPERATURE\","
+    "\"temperatureUnits\":\"CELSIUS\","
+    "\"hvacState\":\"HEATER_ON\","
+    "\"mode\":\"HEATING\","
+    "\"error\":\"THERMOSTAT_OK\"}", 
+    buffer);
+
     
 
-    STRCMP_EQUAL("{\"currentTemperature\":22.1,\"currentHumidity\":50.1}", buffer);
 }
